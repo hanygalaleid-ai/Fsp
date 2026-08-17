ad09b859e07da6d36c0d4209aeae903f81a07145
hanygalaleid-ai <hanygalaleid@gmail.com>
2026-08-17 21:47:51 +0300

    Fix duplicate lobby start event subscription

--- Assets/Fsp/Scripts/Lobby/LobbyMatchLauncher.cs
+++ Assets/Fsp/Scripts/Lobby/LobbyMatchLauncher.cs
@@ -6,23 +6,22 @@ namespace Fsp.Lobby
     public sealed class LobbyMatchLauncher : MonoBehaviour
     {
         [SerializeField] private string battleSceneName = "BattleRoyale";
+        private bool subscribed;
 
-        private void OnEnable()
+        private void Update()
         {
-            if (LobbyState.Instance != null)
+            if (!subscribed && LobbyState.Instance != null)
+            {
                 LobbyState.Instance.StartRequested += HandleStartRequested;
-        }
-
-        private void Start()
-        {
-            if (LobbyState.Instance != null)
-                LobbyState.Instance.StartRequested += HandleStartRequested;
+                subscribed = true;
+            }
         }
 
         private void OnDisable()
         {
-            if (LobbyState.Instance != null)
+            if (subscribed && LobbyState.Instance != null)
                 LobbyState.Instance.StartRequested -= HandleStartRequested;
+            subscribed = false;
         }
 
         private void HandleStartRequested()
