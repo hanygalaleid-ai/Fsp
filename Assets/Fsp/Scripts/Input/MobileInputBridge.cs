416b9459f93984fac0f495aa179fa589c5be2a9e
hanygalaleid-ai <hanygalaleid@gmail.com>
2026-08-17 21:40:33 +0300

    Add mobile input bridge

---
+++ Assets/Fsp/Scripts/Input/MobileInputBridge.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+namespace Fsp.Input
+{
+    public sealed class MobileInputBridge : MonoBehaviour
+    {
+        public static MobileInputBridge Instance { get; private set; }
+
+        public Vector2 Move { get; private set; }
+        public Vector2 Look { get; private set; }
+        public bool FireHeld { get; private set; }
+        public bool AimHeld { get; private set; }
+        public bool SprintHeld { get; private set; }
+        public bool JumpPressed { get; private set; }
+        public bool InteractPressed { get; private set; }
+        public bool SwitchWeaponPressed { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void LateUpdate()
+        {
+            JumpPressed = false;
+            InteractPressed = false;
+            SwitchWeaponPressed = false;
+            Look = Vector2.zero;
+        }
+
+        public void SetMove(Vector2 value) => Move = Vector2.ClampMagnitude(value, 1f);
+        public void AddLook(Vector2 delta) => Look += delta;
+        public void SetFire(bool value) => FireHeld = value;
+        public void SetAim(bool value) => AimHeld = value;
+        public void SetSprint(bool value) => SprintHeld = value;
+        public void PressJump() => JumpPressed = true;
+        public void PressInteract() => InteractPressed = true;
+        public void PressSwitchWeapon() => SwitchWeaponPressed = true;
+    }
+}
