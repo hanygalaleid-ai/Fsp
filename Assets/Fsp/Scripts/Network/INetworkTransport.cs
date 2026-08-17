abbbd3ebf2a44c0d2e5c77c30c86d824dd7c1659
hanygalaleid-ai <hanygalaleid@gmail.com>
2026-08-17 22:34:48 +0300

    Add seat and loot network events

--- Assets/Fsp/Scripts/Network/INetworkTransport.cs
+++ Assets/Fsp/Scripts/Network/INetworkTransport.cs
@@ -9,11 +9,15 @@ namespace Fsp.Networking
         event Action<NetworkFireEvent> FireReceived;
         event Action<NetworkDamageEvent> DamageReceived;
         event Action<NetworkVehicleSnapshot> VehicleReceived;
+        event Action<NetworkSeatEvent> SeatReceived;
+        event Action<NetworkLootClaimEvent> LootClaimReceived;
         void Connect(string matchId, string playerId);
         void Disconnect();
         void SendSnapshot(NetworkPlayerSnapshot snapshot);
         void SendFire(NetworkFireEvent fireEvent);
         void SendDamage(NetworkDamageEvent damageEvent);
         void SendVehicle(NetworkVehicleSnapshot vehicleSnapshot);
+        void SendSeat(NetworkSeatEvent seatEvent);
+        void SendLootClaim(NetworkLootClaimEvent lootClaim);
     }
 }
