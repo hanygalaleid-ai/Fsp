63615b3b43884bfbd04688c421127126da7ea467
hanygalaleid-ai <hanygalaleid@gmail.com>
2026-08-18 00:30:16 +0300

    Wire procedural starter character visuals

--- Assets/Fsp/Scripts/Core/StarterRuntimeFixups.cs
+++ Assets/Fsp/Scripts/Core/StarterRuntimeFixups.cs
@@ -1,6 +1,7 @@
 using Fsp.Backend;
 using Fsp.BattleRoyale;
 using Fsp.Player;
+using Fsp.Presentation;
 using Fsp.Vehicles;
 using UnityEngine;
 
@@ -27,6 +28,8 @@ namespace Fsp.Core
                     player.AddComponent<StarterVehicleInput>();
                 if (player.GetComponent<StarterInteractInput>() == null)
                     player.AddComponent<StarterInteractInput>();
+                if (player.GetComponent<StarterProceduralCharacterVisual>() == null)
+                    player.AddComponent<StarterProceduralCharacterVisual>();
 
                 MatchManager manager = Object.FindObjectOfType<MatchManager>();
                 if (manager != null && manager.GetComponent<MatchProgressReporter>() == null)
