f854f9d94e47efc7c698f7acb3f9a1a20c3f87ee
hanygalaleid-ai <hanygalaleid@gmail.com>
2026-08-17 21:45:22 +0300

    Track killer attribution for kill feed

--- Assets/Fsp/Scripts/Player/PlayerDamageable.cs
+++ Assets/Fsp/Scripts/Player/PlayerDamageable.cs
@@ -1,4 +1,6 @@
+using Fsp.BattleRoyale;
 using Fsp.Combat;
+using Fsp.UI;
 using UnityEngine;
 
 namespace Fsp.Player
@@ -7,15 +9,40 @@ namespace Fsp.Player
     public sealed class PlayerDamageable : MonoBehaviour, IDamageable
     {
         private PlayerVitals vitals;
+        private MatchParticipant victimParticipant;
+        private MatchParticipant lastAttacker;
 
         private void Awake()
         {
             vitals = GetComponent<PlayerVitals>();
+            victimParticipant = GetComponent<MatchParticipant>();
+        }
+
+        private void OnEnable()
+        {
+            if (vitals != null) vitals.Died += HandleDied;
+        }
+
+        private void OnDisable()
+        {
+            if (vitals != null) vitals.Died -= HandleDied;
         }
 
         public void ApplyDamage(float amount, Vector3 point, Vector3 normal, GameObject source)
         {
+            if (source != null)
+            {
+                var attacker = source.GetComponentInParent<MatchParticipant>();
+                if (attacker != null && attacker != victimParticipant)
+                    lastAttacker = attacker;
+            }
+
             vitals.ApplyDamage(amount);
+        }
+
+        private void HandleDied()
+        {
+            KillFeedBus.Report(lastAttacker, victimParticipant);
         }
     }
 }
