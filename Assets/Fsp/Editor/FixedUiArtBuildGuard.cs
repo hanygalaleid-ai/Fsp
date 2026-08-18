63f2bfebc418c34f366a0005400b5beb2c73b71f
hanygalaleid-ai <hanygalaleid@gmail.com>
2026-08-18 20:21:27 +0300

    build: require fixed Sunscar world art

--- Assets/Fsp/Editor/FixedUiArtBuildGuard.cs
+++ Assets/Fsp/Editor/FixedUiArtBuildGuard.cs
@@ -15,7 +15,11 @@ namespace Fsp.EditorTools
             "Assets/Fsp/Art/Resources/UI/joystick_base.png",
             "Assets/Fsp/Art/Resources/UI/ui_panel_dark.png",
             "Assets/Fsp/Art/Resources/UI/ui_button_primary.png",
-            "Assets/Fsp/Art/Resources/UI/ui_button_secondary.png"
+            "Assets/Fsp/Art/Resources/UI/ui_button_secondary.png",
+            "Assets/Fsp/Art/Resources/World/sand_ground.png",
+            "Assets/Fsp/Art/Resources/World/rock_cliff.png",
+            "Assets/Fsp/Art/Resources/World/road_dust.png",
+            "Assets/Fsp/Art/Resources/World/fortress_wall.png"
         };
 
         public void OnPreprocessBuild(BuildReport report)
@@ -26,7 +30,7 @@ namespace Fsp.EditorTools
                     throw new BuildFailedException("Required fixed FSP art is missing: " + path);
 
                 long bytes = new FileInfo(path).Length;
-                if (bytes < 512)
+                if (bytes < 256)
                     throw new BuildFailedException("Required fixed FSP art looks invalid or empty: " + path);
             }
         }
