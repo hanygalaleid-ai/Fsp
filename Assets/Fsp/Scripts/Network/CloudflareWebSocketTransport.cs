df6d7aed0c8bd420bb93538c392ac845ace43a51
hanygalaleid-ai <hanygalaleid@gmail.com>
2026-08-19 20:47:32 +0300

    Send authoritative bot damage through relay

--- Assets/Fsp/Scripts/Network/CloudflareWebSocketTransport.cs
+++ Assets/Fsp/Scripts/Network/CloudflareWebSocketTransport.cs
@@ -76,6 +76,7 @@ namespace Fsp.Networking
         public void SendBotSnapshot(NetworkPlayerSnapshot v) => Send("bot_snapshot", JsonUtility.ToJson(v));
         public void SendFire(NetworkFireEvent v) => Send("fire", JsonUtility.ToJson(v));
         public void SendDamage(NetworkDamageEvent v) => Send("damage", JsonUtility.ToJson(v));
+        public void SendBotDamage(NetworkDamageEvent v) => Send("bot_damage", JsonUtility.ToJson(v));
         public void SendVehicle(NetworkVehicleSnapshot v) => Send("vehicle", JsonUtility.ToJson(v));
         public void SendSeat(NetworkSeatEvent v) => Send("seat", JsonUtility.ToJson(v));
         public void SendLootClaim(NetworkLootClaimEvent v) => Send("loot_claim", JsonUtility.ToJson(v));
